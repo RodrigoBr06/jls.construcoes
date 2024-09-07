a9c712cecc413b0387189a621fefe80758f90499
RodrigoBr06 <163081265+RodrigoBr06@users.noreply.github.com>
2024-09-06 22:06:58 -0300

    .

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function seg(){
     foto.src="https://scontent.cdninstagram.com/v/t51.29350-15/293124355_388106466636937_8757604513731375031_n.webp?stp=dst-jpg&_nc_cat=111&ccb=1-7&_nc_sid=18de74&_nc_ohc=5GdzGuSVXTwQ7kNvgHjmnTm&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AYBjwXeCG2M_BYzVIRIxj6VMPudjQhIsxrv92h7EA1PC3w&oe=66E1886A"
 }
 function volt(){
-    foto.src="https://scontent.cdninstagram.com/v/t51.29350-15/291184536_186597157049301_6278880693105637722_n.webp?stp=dst-jpg&_nc_cat=107&ccb=1-7&_nc_sid=18de74&_nc_ohc=_z_IqaS7z7wQ7kNvgEXSsu1&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AYBWyvp0pDnu0YMyyA7khgzf9QPfQMDbiM8bQqXtoUTDYw&oe=66DAD2D2" 
+    foto.src="https://scontent.cdninstagram.com/v/t51.29350-15/291184536_186597157049301_6278880693105637722_n.webp?stp=dst-jpg&_nc_cat=107&ccb=1-7&_nc_sid=18de74&_nc_ohc=ZxQWg-7w3_UQ7kNvgH-fC8f&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AYDxIIvPln5-XBQ1HvXWS7XxFMX0CTmmdNcpyC7RuxhAFQ&oe=66E16A52" 
 }
 seguir.addEventListener("click",seg)
 voltar.addEventListener("click",volt)
@@ -17,10 +17,10 @@ var voltar2=document.querySelector("#voltar2")
 var foto2=document.querySelector("#img2")
 
 function seg2(){
-    foto2.src="https://scontent.cdninstagram.com/v/t51.29350-15/271429433_289999933181523_1634775262575763692_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=18de74&_nc_ohc=tt0_lvsvoz0Q7kNvgHHSs6M&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&_nc_gid=AtbdG1dh_zWuYzcBLXWZJnu&oh=00_AYD_PmN5TzsJ5cTDLXux4HCplxYGBKAE0yvUTuD62BLWlw&oe=66DAE095"
+    foto2.src="https://scontent.cdninstagram.com/v/t51.29350-15/271429433_289999933181523_1634775262575763692_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=18de74&_nc_ohc=hruVjN7FYZUQ7kNvgEiywna&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AYCDfr-qXZh9w_k9D5eDENduiLPfxch8kbkP3fidktvuZQ&oe=66E17815"
 }
 function volt2(){
-    foto2.src="https://scontent.cdninstagram.com/v/t51.29350-15/271277253_296155895789856_6176664083147839784_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=qBW6cV8zhEgQ7kNvgE9SrRI&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AYB1qHUjrmq7KqhmMy4XlSaZz3jgE9rNpdHwZjDba4uNOg&oe=66DAEC21" 
+    foto2.src="https://scontent.cdninstagram.com/v/t51.29350-15/271277253_296155895789856_6176664083147839784_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=sb9ETn2vqEQQ7kNvgF5FzpF&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AYBUmsIt6nnuD87JVzKK71XiX29vXCffxP3i_jsJkuPWug&oe=66E183A1" 
 }
 
 seguir2.addEventListener("click",seg2)
